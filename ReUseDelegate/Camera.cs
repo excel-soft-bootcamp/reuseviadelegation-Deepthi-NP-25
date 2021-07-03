a0efe480e240a4c16eec302b3856c0ff87f1e32b
Deepthi NP <65824317+Deepthi-NP-25@users.noreply.github.com>
2021-07-03 14:01:12 +0530

    Add files via upload

---
+++ ReUseDelegate/Camera.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ReUseDelegate
+{
+    public class Camera : ICamera
+    {
+        public void Click() 
+        {
+            Console.WriteLine("Click by Camera");
+        }
+
+    }
+}
